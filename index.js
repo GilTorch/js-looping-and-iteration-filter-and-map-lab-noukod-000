93cbcd4bd84a63aee7be6ea336cee1e13b965cfb
Gilbert Torchon <gilbert@noukod.com>
2019-02-07 19:00:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 function driversWithRevenueOver(drivers,revenue){
     return drivers.filter((driver)=>driver.revenue > revenue);
 }
+
+
+function driversNameWithRevenueOver(drivers,revenue){
+  return driverNamesWithRevenueOver(drivers,revenue).map((driver)=>driver.name)
+}
